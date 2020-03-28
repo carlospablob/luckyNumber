14a4aa74a88a470bc01b29cd5006a9a8f5699cc2
Carlos Pablo <carlos.pablo@betechcapital.com>
2020-03-27 21:02:10 -0600

    feat: Maquetado, Implementacion de logica para calcular el numero de la suerte

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'calculateLucky';
+export class AppComponent implements OnInit {
+  formLucky: FormGroup;
+
+  numLucky: number;
+
+  constructor(
+    private fb: FormBuilder
+  ) {  }
+
+  ngOnInit(): void {
+    this.loadForm();
+  }
+
+  loadForm() {
+    // Se crea un formulario para asociar al campos mas sus validaciones
+    this.formLucky = this.fb.group({
+      number: [null, [
+        Validators.required,
+        Validators.minLength(14),
+        Validators.maxLength(16),
+        Validators.pattern(`^[0-9]+$`),
+      ]]
+    });
+  }
+
+  getNumberLucky() {
+    // Se crea un arreglo apartir del string ingresado
+    const arrayNumbers = this.formLucky.get('number').value.split('');
+    let sum = 0;
+    let num = 0;
+    let total = 0;
+
+    // Se obtiene arreglo de valores unicos
+    const unique = arrayNumbers.filter((elem, index, self) =>  {
+      return index === self.indexOf(elem);
+    });
+
+    // Se recorre valores unicos
+    for(let cont = 0; cont < unique.length; cont++) {
+      // Se recorre los valores del arreglo inicial
+      for(let cont2 = 0; cont2 < arrayNumbers.length; cont2++) {
+        // Se calcula el total de veces que aparece el numero
+        if (arrayNumbers[cont2] === unique[cont]) {
+          sum = sum + 1;
+        }
+      }
+      // Se verifica si la suma actual es mayor al total almacenado, si es mayor se sustituye
+      if (total < sum) {
+        total = sum;
+        num = unique[cont];
+      }
+      // Se resetea el valor de veces que aparece el numero
+      sum = 0;
+    }
+    console.log('Numero que mas se repite', num);
+    console.log('Numero de veces que se repite', total);
+
+    this.numLucky = num;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule, MatButtonModule, MatSelectModule, MatIconModule, MatToolbarModule } from '@angular/material';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    FormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatIconModule,
+    MatToolbarModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
